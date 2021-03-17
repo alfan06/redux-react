59c1543fd25eee7349f5a5945cb4a5529504b073
alfan06 <56281741+alfan06@users.noreply.github.com>
2021-03-17 12:00:41 +0700

    modul 6

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from "react-redux";
+import { createStore, compose } from "redux";
+import MainReducer from "./reducers/MainReducer";
 import reportWebVitals from './reportWebVitals';
+import Table from "../src/containers/Table";
+import CreateTodo from "../src/containers/CreateTodo";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = compose(
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f
+)(createStore)(MainReducer);
+  
+  ReactDOM.render(
+    <Provider store={store}>
+      <CreateTodo />
+      <Table />
+    </Provider>,
+    document.getElementById("root")
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
